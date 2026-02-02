9e9d5d2d0e3d7a88e7e0021963c2d8d6aff90522
Intikhab Ansari <intikhab.ansari@kantar.com>
2026-02-02 22:29:47 +0530

    product service code

---
+++ product-service/product-service/src/main/java/com/grocery/product_service/entity/Product.java
@@ -0,0 +1,19 @@
+package com.grocery.product_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String description;
+    private double price;
+
+}
